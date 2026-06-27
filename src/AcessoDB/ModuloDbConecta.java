2e223077b99fa51008deb882973fa4df4a1d2f49
JGuilherme <4555854518@estudante.sed.sc.gov.br>
2026-06-27 16:39:59 -0300

    feat: exclusão dos comentários do acesso ao banco

--- src/AcessoDB/ModuloDbConecta.java
+++ src/AcessoDB/ModuloDbConecta.java
@@ -4,32 +4,20 @@ package AcessoDB;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class ModuloDbConecta {
-    
-    // 1o. - Passo: definir as variáveis da String de Conexão
-    public static Connection connector() {
-        // Definindi as variáveis
-        java.sql.Connection conexao = null; // Recebe o resultado da conexão. 
-        // Definir o Driver para o SGBD a ser conectado
-        String driver = "com.mysql.cj.jdbc.Driver";
-        // Definir as demais variáveis da String de conexão
+public class ModuloDbConecta {    
+    public static Connection connector() {   
+        java.sql.Connection conexao = null;         
+        String driver = "com.mysql.jdbc.Driver";      
         String url = "jdbc:mysql://127.0.0.1:3306/db_chronos";
         String user = "root";
-        String senha = "1234";
-        
-        // 2 - Fazer a conexao ao banco
-        try {
-            // Acionando/chamando o driver para fazer a conexão
+        String senha = "";            
+        try {          
             Class.forName(driver);
-            conexao = DriverManager.getConnection(url, user, senha);
-            // Se conectou OK!!
-            // retornar o resultado da conexão!!!
+            conexao = DriverManager.getConnection(url, user, senha);           
             return conexao;
-        }catch (Exception varERRO) {
-            // caso haja erro, retorna com "null"
+        }catch (Exception varERRO) {            
             System.out.println(" ERRO gerado na conexão ao banco: " + varERRO.toString());
             return null;
         }
-    }
-    
+    }    
 }
